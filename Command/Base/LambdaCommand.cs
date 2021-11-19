63b01fe5329ca44b818302af0b42547e8a24281b
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-19 12:58:51 +0300

    Базовая команда Лямбда команда и Команда выхода

---
+++ Command/Base/LambdaCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Command.Base
+{
+    class LambdaCommand : CommandBase
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
