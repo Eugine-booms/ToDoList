db252d96407d196f1d2652189045d4607dd72a44
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-25 12:51:18 +0300

    host

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Globalization;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Globalization;
 using System.Windows;
 
 namespace ToDoList
@@ -9,10 +12,38 @@ namespace ToDoList
     public partial class App : Application
     {
         public static bool IsDesigneMode { get; private set; } = true;
-        protected override void OnStartup(StartupEventArgs e)
+
+        private static IHost host;
+
+        public static IHost Host
+        {
+            get
+            {
+                if (host != null) return host;
+                host = Programm.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+                return host;
+            }
+        }
+        protected override async void OnStartup(StartupEventArgs e)
         {
             IsDesigneMode = false;
+            var host = Host;
             base.OnStartup(e);
+            await host.StartAsync().ConfigureAwait(false);
         }
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host = null;
+        }
+
+        internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.RegisterServices()
+                .RegisterViewModels();
+        }
+        
     }
 }
---
+++ Programm.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+public static class Programm
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+         Host.CreateDefaultBuilder(args)
+             .ConfigureServices((hostContext, services) =>
+             {
+                 services.AddHostedService<Worker>();
+             });
+    }
+}
